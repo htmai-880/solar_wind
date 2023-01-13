0f2a5f04b543a71e39ddb7314c7a4330a8e20dba
HT MAI <huutan.mai@gmail.com>
2023-01-13 18:18:41 +0100

    Added first model

--- submissions/attention_1/estimator.py
+++ submissions/attention_1/estimator.py
@@ -4,6 +4,8 @@ from sklearn.base import BaseEstimator
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LogisticRegression
 
+import gc
+
 import torch
 import torch.nn as nn
 import numpy as np
@@ -114,26 +116,33 @@ class PositionalEncoding(nn.Module):
 
 
 class MyDataset(Dataset):
-    def __init__(self, X, y=None, max_history=64):
+    def __init__(self, X, y=None, max_len=64):
         self.X = X
+        self.y = y
         if not y is None:
-            self.y = y
-        self.max_history = max_history
+            # print("Built dataset. len y:", len(y))
+            self.max_history = max_len-1
 
     def __len__(self):
         return len(self.X)
 
     def __getitem__(self, index):
+        # Get data (x, y)
+
         start = max(0, index-self.max_history)
-        
-        sequence = self.X.iloc[start:index]
-        # print(sequence)
+        # sequence includes X.iloc[index]
+        sequence = self.X.iloc[start:index+1]
+        # print("index: ", index)
+        # print("Sequence :\n", sequence)
         sequence = torch.tensor(sequence.values)
         if not self.y is None:
-            label = torch.tensor([self.y[index+1]], dtype=torch.int64)
+            # print(self.y)
+            label = self.y[index]
+            # print("label : ", label)
+            label = torch.tensor([label], dtype=torch.int64)
             return sequence, label
         else:
-            return sequence
+            return (sequence,)
 
 
 def MyCollator(batch):
@@ -141,13 +150,15 @@ def MyCollator(batch):
     # print("batch item = \n", batch[0][0].size())
     source_sequences = pad_sequence([sample[0] for sample in batch])
     # print("label item = ", batch[0][1])
-    labels = pad_sequence([torch.tensor([sample[1]]) for sample in batch])
+    if len(batch[0]) == 1:
+        return (source_sequences,)
 
+    labels = pad_sequence([torch.tensor([sample[1]]) for sample in batch])
     return source_sequences, labels.reshape(-1)
 
 
 def get_loader(X, y=None, max_len=64, batch_size=32):
-    dataset = MyDataset(X, y, max_history=max_len)
+    dataset = MyDataset(X, y, max_len=max_len)
     data_loader = DataLoader(dataset=dataset, batch_size=batch_size,
                              collate_fn=MyCollator, pin_memory=True, drop_last=True)
     return data_loader
@@ -158,7 +169,8 @@ def get_loader(X, y=None, max_len=64, batch_size=32):
 
 
 def train_batch(model, data, optimizer, criterion):
-    device = next(model.parameters()).device
+    params = next(model.parameters())
+    device = params.device
 
     batch_loss = 0
 
@@ -180,16 +192,29 @@ def train_batch(model, data, optimizer, criterion):
 
     optimizer.step()
     batch_loss += loss.item()
+
+    # cleanup memory
+    del output, target
+    gc.collect()
+    if params.is_cuda:
+        torch.cuda.empty_cache()
+
     return batch_loss
 
 
 def train_epoch(model, X_train, y_train, optimizer, criterion, max_len=64, log_interval=5, batch_size=32):
     model.train()
     total_loss = 0
+    # print(y_train)
     data_loader = get_loader(
         X_train, y_train, max_len=max_len, batch_size=batch_size)
     losses = []
     for idx, data in enumerate(data_loader):
+        # print("--- New batch ---")
+        # print("len y in this batch: ", len(y_train))
+        # print("idx: ", idx)
+        # print(data[0].size())
+        # print(data[1].size())
         batch_loss = train_batch(model, data, optimizer, criterion)
         total_loss += batch_loss
         if idx % log_interval == 0 and idx > 0:
@@ -202,6 +227,8 @@ def train_epoch(model, X_train, y_train, optimizer, criterion, max_len=64, log_i
             )
             losses.append(cur_loss)
             total_loss = 0
+
+        # print("--- Finished batch ---")
     return losses
 
 ##########################################################
@@ -236,27 +263,27 @@ class Classifier(BaseEstimator):
         self.optimizer = torch.optim.Adam(
             self.model.parameters(), lr=self.lr)
         
-        X = X.iloc[:250000]
-        y = y.iloc[:250000]
+        # X = X.iloc[:100]
+        # y = y.iloc[:100]
 
         log_interval = 50
         for epoch in range(1, self.epochs+1):
-            print("----- Epoch " + str(epoch) + " -----")
+            print("----------- Epoch " + str(epoch) + " -----------")
             train_epoch(self.model, X, y, self.optimizer, self.criterion,
                         max_len=self.max_len, log_interval=log_interval, batch_size=self.batch_size)
-        torch.save(self.model.state_dict(), './best_model.pt')
+            torch.save(self.model.state_dict(), './best_model.pt')
 
 
     def predict(self, X):
         self.model.eval()
         get_loader(X, None, max_len=self.max_len, batch_size=self.batch_size)
         y = []
-        data = data_loader = get_loader(
-            X, y, max_len=self.max_len, batch_size=self.batch_size)
+        data_loader = get_loader(
+            X, y=None, max_len=self.max_len, batch_size=self.batch_size)
         for idx, data in enumerate(data_loader):
             src_mask = self.model.base.generate_square_subsequent_mask(
                 data[0].size(0)).to(self.device)
-            input_ = data[0].to(self.device)
+            input_ = data[0].float().to(self.device)
             y.append(self.model(input_, src_mask))
         y_pred = torch.cat(y, dim=0)
         return y_pred.numpy()
@@ -265,11 +292,11 @@ class Classifier(BaseEstimator):
 def get_estimator():
 
     nhead = 4
-    nhid = 256
-    nlayers = 4
-    dropout = 0.2
-    epochs = 1
-    batch_size = 32
+    nhid = 128
+    nlayers = 3
+    dropout = 0.3
+    epochs = 2
+    batch_size = 8
     lr = 1e-3
 
     feature_extractor = FeatureExtractor()
